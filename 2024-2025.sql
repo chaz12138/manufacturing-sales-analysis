a31fb2dc012b565fa5895c5d4c7dc67c64c9ea31
chaz12138 <2486540095@qq.com>
2026-02-07 14:21:00 +0800

    Update 2024-2025.sql

--- 2024-2025.sql
+++ 2024-2025.sql
@@ -1,7 +1,7 @@
 SELECT 
-    strftime('%Y', 日期) as 年份,
+    DATE_FORMAT(日期, '%Y') as 年份,
     ROUND(SUM(金额)) as 年销售额,
     COUNT(*) as 订单笔数
 FROM 订单表
-GROUP BY strftime('%Y', 日期)
+GROUP BY DATE_FORMAT(日期, '%Y')
 ORDER BY 年份;
